50334596e9cafd5d0ecfe856f8ca7c60ad88c7a8
sahil patel <sahil.patel@techivies.com>
2024-06-17 11:45:35 +0530

    ad

--- src/components/Catalogue.tsx
+++ src/components/Catalogue.tsx
@@ -1,7 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
 
 const Catalogue = () => {
-    const [showContent, setShowContent] = useState(false);
     function handleDownload() {
         const pdfFilePath = "/assets/pdf/Catalogue_sample.pdf";
 
@@ -15,41 +14,30 @@ const Catalogue = () => {
         document.body.removeChild(link);
     }
 
-    function toggleContent() {
-        setShowContent(!showContent);
-    }
-
     return (
         <>
-            <div className="mb-3 w-screen flex justify-center items-center">
-                <button className="tempcolor" onClick={toggleContent}>
-                    {showContent ? "Show Less" : "Read More"}
-                </button>
-            </div>
-            {
-                showContent && (<div className="wrapper bg-[#00000010] p-5 mb-[20px]">
-                    <div className="company-hover">
-                        <div className="row gx-5">
-                            <div className="col-md-8 d-flex align-items-center ">
-                                <div className="text-wrapper">
-                                    <h6 className="text-uppercase text-lg">
-                                        DOWNLOAD LATEST COLLECTION’S CATALOGUE from Flowless
-                                        International
-                                    </h6>
-                                    <p className="mt-4">
-                                        Experience the unparalleled elegance and versatility of
-                                        Flowless International&apos;s premium tile and stone
-                                        collections by downloading our comprehensive catalogue today.
-                                    </p>
-                                    <div className="mt-[30px] tempcolor">
-                                        <button onClick={handleDownload}>Download Catalogue</button>
-                                    </div>
+            <div className="wrapper bg-[#00000010] p-5 mb-[20px]">
+                <div className="company-hover">
+                    <div className="row gx-5">
+                        <div className="col-md-8 d-flex align-items-center ">
+                            <div className="text-wrapper">
+                                <h6 className="text-uppercase text-lg">
+                                    DOWNLOAD LATEST COLLECTION’S CATALOGUE from Flowless
+                                    International
+                                </h6>
+                                <p className="mt-4">
+                                    Experience the unparalleled elegance and versatility of
+                                    Flowless International&apos;s premium tile and stone
+                                    collections by downloading our comprehensive catalogue today.
+                                </p>
+                                <div className="mt-[30px] tempcolor">
+                                    <button onClick={handleDownload}>Download Catalogue</button>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>)
-            }
+                </div>
+            </div>
         </>
     );
 };
